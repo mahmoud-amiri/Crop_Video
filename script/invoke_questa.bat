edfd892249dd5120057bf3bcfc22ae73720a1237
mahmoud amiri <itsmahmoudamiri@gmail.com>
2024-06-14 10:37:39 +0200

    modify invoke questa file

--- script/invoke_questa.bat
+++ script/invoke_questa.bat
@@ -1,5 +1,6 @@
 :: crop_video
 @set QUESTA_ROOT=C:\questasim64_2021.1
 @set UVMF_HOME=C:\UVMF_2023.4\UVMF_2023.4
-vsim -i -do "do compile.do; do run.do
+cd .\tb\uvmf\project_benches\crop_video\sim
+vsim -c -do "do compile.do; do run.do
 pause
